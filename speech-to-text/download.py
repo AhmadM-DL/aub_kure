954826807e358c33b403243714241549fa2b7b78
AhmadM-DL <52525688+AhmadM-DL@users.noreply.github.com>
2025-04-25 16:18:27 +0300

    Update download.py

--- speech-to-text/download.py
+++ speech-to-text/download.py
@@ -3,7 +3,7 @@ import whisper
 if __name__ == "__main__":
     try:
         print("Downloading Whisper model...")
-        model = whisper.load_model("base", download_root="/root/.cache/whisper")
+        model = whisper.load_model("base.en", download_root="/root/.cache/whisper")
         print("Whisper model cached successfully at /root/.cache/whisper")
     except Exception as e:
         print(f"Failed to download model: {e}")
